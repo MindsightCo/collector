ddcdfa2b8ec91154268fbd04a878353768535fc7
Eddy Reyes <ereyes01@users.noreply.github.com>
2019-06-27 14:58:24 -0500

    Implement replacing sources in the cache

--- cache/cache.go
+++ cache/cache.go
@@ -2,6 +2,7 @@ package cache
 
 import (
 	promclient "github.com/MindsightCo/metrics-agent/prometheus_client"
+	"github.com/pkg/errors"
 	prommodel "github.com/prometheus/common/model"
 )
 
@@ -13,8 +14,25 @@ type Source struct {
 }
 
 type Cache struct {
-	source  *Source
+	sources []Source
 	values  map[int]prommodel.Vector
-	limit   int
-	nCached int
+}
+
+func (c *Cache) NewSources(sources []Source) (map[int]prommodel.Vector, error) {
+	sourcesCopy := append([]Source{}, sources...)
+	for idx, src := range sourcesCopy {
+		client, err := promclient.NewPromClient(src.URL)
+		if err != nil {
+			// TODO: close all previous connections?
+			return nil, errors.Wrap(err, "connect to prometheus server")
+		}
+
+		sourcesCopy[idx].client = client
+	}
+
+	prevValues := c.values
+	c.values = make(map[int]prommodel.Vector)
+	c.sources = sourcesCopy
+
+	return prevValues, nil
 }
--- cache/cache_test.go
+++ cache/cache_test.go
@@ -2,7 +2,59 @@ package cache
 
 import (
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	prommodel "github.com/prometheus/common/model"
 )
 
+var epoch = prommodel.TimeFromUnix(10)
+
+func TestNewSources(t *testing.T) {
+	prevValues := map[int]prommodel.Vector{
+		23: prommodel.Vector{
+			&prommodel.Sample{
+				Timestamp: 0,
+				Value:     prommodel.SampleValue(13.3),
+				Metric: prommodel.Metric{
+					"__name__": "fred",
+				},
+			},
+		},
+	}
+
+	c := &Cache{
+		sources: []Source{{SourceID: 2}},
+		values:  prevValues,
+	}
+
+	newSources := []Source{{SourceID: 3, URL: "a-url"}}
+
+	values, err := c.NewSources(newSources)
+	if err != nil {
+		t.Fatal("set new sources:", err)
+	}
+	if !cmp.Equal(values, prevValues) {
+		t.Fatalf("unexpected values returned, diff: %s", cmp.Diff(values, prevValues))
+	}
+
+	ignoreUnexp := cmpopts.IgnoreUnexported(Source{})
+
+	if !cmp.Equal(c.sources, newSources, ignoreUnexp) {
+		t.Fatalf("unexpected sources in cache: %s", cmp.Diff(c.sources, newSources, ignoreUnexp))
+	}
+	if len(c.values) != 0 {
+		t.Fatal("unexpected values in cache after new sources:", c.values)
+	}
+
+	for _, src := range c.sources {
+		if src.client == nil {
+			t.Fatal("found a source without a prometheus client:", src)
+		}
+	}
+}
+
 func TestCache(t *testing.T) {
+	// needs a func that runs all the queries in its sources and caches the results,
+	// potentially flushing the cache
 }
