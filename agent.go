544f270e304854632b64eda5495820a73590aa9d
Eddy Reyes <ereyes01@users.noreply.github.com>
2019-07-01 12:18:08 -0500

    Add main loop

--- agent.go
+++ agent.go
@@ -2,37 +2,7 @@ package main
 
 import (
 	"log"
-
-	auth0grant "github.com/ereyes01/go-auth0-grant"
-	"github.com/pkg/errors"
 )
-
-const (
-	credsAudience = "https://api.mindsight.io/"
-	auth0TokenURL = "https://mindsight.auth0.com/oauth/token/"
-)
-
-func initGrant(c *Config) (*auth0grant.Grant, error) {
-	if c.TestMode {
-		return nil, nil
-	}
-
-	credRequest := auth0grant.CredentialsRequest{
-		ClientID:     c.ClientID,
-		ClientSecret: c.ClientSecret,
-		Audience:     credsAudience,
-		GrantType:    auth0grant.CLIENT_CREDS_GRANT_TYPE,
-	}
-
-	grant := auth0grant.NewGrant(auth0TokenURL, credRequest)
-
-	// test the token
-	if _, err := grant.GetAccessToken(); err != nil {
-		return nil, errors.Wrap(err, "testing credentials")
-	}
-
-	return grant, nil
-}
 
 func main() {
 	config, err := ReadConfig()
@@ -40,8 +10,5 @@ func main() {
 		log.Fatal("error verifying config:", err)
 	}
 
-	_, err = initGrant(config)
-	if err != nil {
-		log.Fatal("auth error:", err)
-	}
+	log.Fatal(config.Loop())
 }
--- config.go
+++ config.go
@@ -1,28 +1,43 @@
 package main
 
 import (
+	"context"
 	"log"
 	"time"
 
+	"github.com/MindsightCo/metrics-agent/apiclient"
 	"github.com/MindsightCo/metrics-agent/cache"
+	auth0grant "github.com/ereyes01/go-auth0-grant"
 	"github.com/pkg/errors"
 	"github.com/spf13/viper"
 )
 
 const (
-	defaultAPIServer  = "https://sre-api.mindsight.io/"
-	defaultCacheDepth = 1000
-	defaultCacheAge   = time.Minute * 5
+	defaultAPIServer              = "https://sre-api.mindsight.io/"
+	defaultCacheDepth             = 1000
+	defaultCacheAge               = time.Minute * 5
+	defaultScrapeInterval         = time.Second * 5
+	defaultRefreshSourcesInterval = time.Hour
+
+	credsAudience = "https://api.mindsight.io/"
+	auth0TokenURL = "https://mindsight.auth0.com/oauth/token/"
 )
 
 type Config struct {
-	Sources      []cache.Source
-	ClientID     string        `mapstructure:"client_id"`
-	ClientSecret string        `mapstructure:"client_secret"`
-	APIServer    string        `mapstructure:"api_server"`
-	TestMode     bool          `mapstructure:"test_mode"`
-	CacheAge     time.Duration `mapstructure:"cache_age"`
-	CacheDepth   int           `mapstructure:"cache_depth"`
+	Sources                []cache.Source
+	ClientID               string        `mapstructure:"client_id"`
+	ClientSecret           string        `mapstructure:"client_secret"`
+	APIServer              string        `mapstructure:"api_server"`
+	TestMode               bool          `mapstructure:"test_mode"`
+	CacheAge               time.Duration `mapstructure:"cache_age"`
+	CacheDepth             int           `mapstructure:"cache_depth"`
+	ScrapeInterval         time.Duration `mapstructure:"scrape_interval"`
+	RefreshSourcesInterval time.Duration `mapstructure:"refresh_sources_interval"`
+
+	auth    *auth0grant.Grant
+	cache   *cache.Cache
+	pusher  *apiclient.MetricsPusher
+	queryer *apiclient.Queryer
 }
 
 // ReadConfig retrieves configuration values via viper. If a required
@@ -41,6 +56,8 @@ func ReadConfig() (*Config, error) {
 	viper.SetDefault("api_server", defaultAPIServer)
 	viper.SetDefault("cache_age", defaultCacheAge)
 	viper.SetDefault("cache_depth", defaultCacheDepth)
+	viper.SetDefault("scrape_interval", defaultScrapeInterval)
+	viper.SetDefault("refresh_sources_interval", defaultRefreshSourcesInterval)
 
 	// loads viper config
 	err := viper.ReadInConfig()
@@ -60,4 +77,129 @@ func ReadConfig() (*Config, error) {
 	}
 
 	return &c, nil
+}
+
+func (c *Config) initAuth() error {
+	credRequest := auth0grant.CredentialsRequest{
+		ClientID:     c.ClientID,
+		ClientSecret: c.ClientSecret,
+		Audience:     credsAudience,
+		GrantType:    auth0grant.CLIENT_CREDS_GRANT_TYPE,
+	}
+
+	grant := auth0grant.NewGrant(auth0TokenURL, credRequest)
+
+	// test the token
+	if _, err := grant.GetAccessToken(); err != nil {
+		return errors.Wrap(err, "testing credentials")
+	}
+
+	c.auth = grant
+	return nil
+}
+
+func (c *Config) init() error {
+	if err := c.initAuth(); err != nil {
+		return errors.Wrap(err, "init auth")
+	}
+
+	cache, err := cache.NewCache(c.Sources, c.CacheDepth, c.CacheAge)
+	if err != nil {
+		return errors.Wrap(err, "init cache")
+	}
+
+	pusher, err := apiclient.NewMetricsPusher(c.APIServer, c.auth)
+	if err != nil {
+		return errors.Wrap(err, "init metrics pusher")
+	}
+
+	queryer, err := apiclient.NewQueryer(c.APIServer, c.auth)
+	if err != nil {
+		return errors.Wrap(err, "init queryer")
+	}
+
+	c.cache = cache
+	c.pusher = pusher
+	c.queryer = queryer
+
+	if err := c.refreshSources(context.Background()); err != nil {
+		return errors.Wrap(err, "init refresh sources")
+	}
+
+	return nil
+}
+
+func (c *Config) scrape(ctx context.Context) error {
+	data, err := c.cache.Collect(ctx)
+	if err != nil {
+		return errors.Wrap(err, "scrape")
+	}
+
+	if data != nil {
+		if err := c.pusher.Push(ctx, data); err != nil {
+			return errors.Wrap(err, "push from scrape")
+		}
+	}
+
+	return nil
+}
+
+func (c *Config) refreshSources(ctx context.Context) error {
+	sources, err := c.queryer.QuerySources(ctx)
+	if err != nil {
+		return errors.Wrap(err, "query sources")
+	}
+
+	data, err := c.cache.NewSources(sources)
+	if err != nil {
+		return errors.Wrap(err, "set new sources")
+	}
+
+	if data != nil {
+		if err := c.pusher.Push(ctx, data); err != nil {
+			return errors.Wrap(err, "push after refresh sources")
+		}
+	}
+
+	return nil
+}
+
+// resetTimer according to the Timer docs' instructions.
+// https://golang.org/pkg/time/#Timer.Reset
+func resetTimer(t *time.Timer, d time.Duration) {
+	if !t.Stop() {
+		<-t.C
+	}
+	t.Reset(d)
+}
+
+func (c *Config) Loop() error {
+	if err := c.init(); err != nil {
+		return errors.Wrap(err, "init metrics collector")
+	}
+
+	scrapeTimer := time.NewTimer(c.ScrapeInterval)
+	refreshSourcesTimer := time.NewTimer(c.RefreshSourcesInterval)
+	ctx := context.Background()
+
+	for {
+		select {
+		case <-scrapeTimer.C:
+			if err := c.scrape(ctx); err != nil {
+				log.Println("WARNING (scrape):", err)
+			}
+			resetTimer(scrapeTimer, c.ScrapeInterval)
+
+		// TODO: instead of a timer, replace with a subscription notification
+		// from the API
+		case <-refreshSourcesTimer.C:
+			if err := c.refreshSources(ctx); err != nil {
+				log.Println("WARNING (refreshSources):", err)
+			}
+			resetTimer(refreshSourcesTimer, c.RefreshSourcesInterval)
+		}
+	}
+
+	// should never get here
+	return nil
 }
